deb1f6c0cda6678816f3d07e99d6a571a12ad734
shabinder <owner@shabinder.in>
2021-09-02 17:23:16 +0530

    FFmpeg-android Optimisations

--- scripts/ffmpeg/build.sh
+++ scripts/ffmpeg/build.sh
@@ -26,33 +26,15 @@ DEP_CFLAGS="-I${BUILD_DIR_EXTERNAL}/${ANDROID_ABI}/include"
 DEP_LD_FLAGS="-L${BUILD_DIR_EXTERNAL}/${ANDROID_ABI}/lib $FFMPEG_EXTRA_LD_FLAGS"
 
 ./configure \
+  --disable-everything \
   --disable-doc \
   --disable-network \
   --disable-autodetect \
+  --disable-filters \
   --disable-swscale \
-  --disable-muxers \
-  --disable-demuxers \
-  --disable-zlib \
-  --disable-bzlib \
-  --disable-iconv \
-  --disable-bsfs \
-  --disable-filters \
-  --disable-parsers \
-  --disable-indevs \
-  --disable-outdevs \
-  --disable-decoders \
-  --disable-encoders \
-  --disable-hwaccels \
-  --disable-protocols \
-  --disable-nvenc \
-  --disable-xvmc \
-  --disable-videotoolbox \
-  --disable-audiotoolbox \
-  --disable-filters \
+  --disable-avdevice \
+  --disable-ffprobe \
   --enable-small \
-  --enable-pthreads \
-  --enable-gpl \
-  --enable-pic \
   --prefix=${BUILD_DIR_FFMPEG}/${ANDROID_ABI} \
   --enable-cross-compile \
   --target-os=android \
@@ -68,9 +50,7 @@ DEP_LD_FLAGS="-L${BUILD_DIR_EXTERNAL}/${ANDROID_ABI}/lib $FFMPEG_EXTRA_LD_FLAGS"
   --strip=${FAM_STRIP} \
   --extra-cflags="-O3 -fPIC $DEP_CFLAGS" \
   --extra-ldflags="$DEP_LD_FLAGS" \
-  --enable-parser=aac*,mpegaudio \
   --enable-filter=aresample \
-  --enable-bsf=aac_adtstoasc \
   --enable-decoder=aac*,ac3* \
   --enable-encoder=libmp3lame \
   --enable-demuxer=mov \
